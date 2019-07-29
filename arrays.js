ee37b0103831513e1f87aef22fa16ddce0864796
Rayshawn Woolard <r.woolard456@gmail.com>
2019-07-29 15:20:05 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,3 +23,9 @@ var destructivelyAddElementToEndOfArray = (array, element) => {
 function accessElementInArray(array, index) {
   return array[index];
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift;
+  return array;
+}
+
